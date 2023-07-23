2fa0f414cd958b3498590e222b800a411f6f9e55
geoffserv <gtraugott@gmail.com>
2023-07-23 14:38:03 -0600

    Multi chassis support and way cool frontend

--- cias_control.py
+++ cias_control.py
@@ -6,15 +6,18 @@ Requirements
 ------------
 telnetlib : Simple telnet library
 cias_globals : Program globals
+time : to sleep etc
 """
 
 import telnetlib
 import cias_globals
+import time
 
 
 class CiasControl(object):
-    def __init__(self):
-        self.host = cias_globals.config['cias'].get('CrestronHostname')
+    def __init__(self, chassis):
+        self.chassis_num = chassis
+        self.host = cias_globals.config['cias'].get('c{}hostname'.format(self.chassis_num))
 
         # Assuming a DM-MD8x8, because that's what I have
         # Build an array to reference the card names vs chassis #s
@@ -23,13 +26,17 @@ class CiasControl(object):
         # u know
         self.cardsInput = []
         for i in range(1, 9):
-            self.cardsInput.append([cias_globals.config['cias'].get('input{}Name'.format(i)),
-                                    cias_globals.config['cias'].get('input{}Card'.format(i))])
+            self.cardsInput.append([cias_globals.config['cias'].get('c{}input{}Name'.format(self.chassis_num, i)),
+                                    cias_globals.config['cias'].get('c{}input{}Card'.format(self.chassis_num, i))])
 
         self.cardsOutput = []
         for i in range(1, 9):
-            self.cardsOutput.append([cias_globals.config['cias'].get('output{}Name'.format(i)),
-                                     cias_globals.config['cias'].get('output{}Card'.format(i))])
+            self.cardsOutput.append([cias_globals.config['cias'].get('c{}output{}Name'.format(self.chassis_num, i)),
+                                     cias_globals.config['cias'].get('c{}output{}Card'.format(self.chassis_num, i))])
+
+        self.lcdHeader = cias_globals.config['cias'].get('c{}lcdHeader'.format(self.chassis_num))
+        self.lcdSeparator = cias_globals.config['cias'].get('c{}lcdSeparator'.format(self.chassis_num))
+        self.lcdTagLine = cias_globals.config['cias'].get('c{}lcdTagLine'.format(self.chassis_num))
 
     def send_command(self, telnet_cmd):
         # TODO: validate input
@@ -56,13 +63,30 @@ class CiasControl(object):
         telnet.close()
 
     def lcd_update(self, message):
-        # Combine the message with a separator and a tagline from the config file
-        lcd_message = "{} {} {} {}".format(cias_globals.config['cias'].get('lcdHeader'),
-                                           message,
-                                           cias_globals.config['cias'].get('lcdSeparator'),
-                                           cias_globals.config['cias'].get('lcdTagLine'))
         # Send it to the device LCD
-        self.send_command("MESSage {}\r".format(lcd_message))
+        self.send_command("MESSage {}\r".format(message))
+
+    def lcd_format(self, message):
+        # Combine the message with a separator and a tagline from the config file
+        lcd_message = "{} {} {} {}".format(self.lcdHeader,
+                                           message,
+                                           self.lcdSeparator,
+                                           self.lcdTagLine)
+        # Send it to the device LCD
+        self.lcd_update(lcd_message)
+
+    def lcd_test(self):
+        # Do a LCD char test
+        # Hack crap just cause I'm interested in MD8x8's capabilities
+        # turns out it's all ascii under 128
+        test_output = ""
+        for abatch in range(0,13):
+            for adigit in range(0,10):
+                if ((abatch*10)+adigit < 128):
+                    test_output = "{}, {}:{}".format(test_output, (abatch*10)+adigit, chr((abatch*10)+adigit))
+            self.lcd_update(test_output)
+            time.sleep(2)
+            test_output = ""
 
     def get_input_card_name(self, card_input):
         # return the pretty name of an input card
@@ -81,11 +105,5 @@ class CiasControl(object):
                                        self.cardsOutput[card_output][0])
         self.send_command("SETAVROUTE {} {}\r".format(self.cardsInput[card_input][1],
                                                       self.cardsOutput[card_output][1]))
-        self.lcd_update(lcd_message)
+        self.lcd_format(lcd_message)
 
-
-# if __name__ == "__main__":
-#     cias = CiasControl()
-#
-#     # Route input 1 to output 1
-#     cias.route_av(1, 1)
